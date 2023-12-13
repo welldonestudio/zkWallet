00d445765dc4ec2517826c7941f1e1587246788a
daoauth <57783762+daoauth@users.noreply.github.com>
2023-12-13 15:48:40 +0900

    Update index.tsx

--- component/pages/signup/callback/index.tsx
+++ component/pages/signup/callback/index.tsx
@@ -108,7 +108,7 @@ export const SignUpCallbackPage = () => {
           jwt: id_token as string,
         }),
       );
-      createWallet(id_token as string);
+      // createWallet(id_token as string);
     } catch (err) {
       console.log(err);
       setTimeout(() => setError(true), 300);
