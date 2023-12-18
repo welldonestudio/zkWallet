99efbeb33ab56cf92e18e302728a56bd67c4791f
daoauth <57783762+daoauth@users.noreply.github.com>
2023-12-18 18:59:39 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui.js/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SnackbarProvider } from 'notistack';
 import { useStore } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import '@mysten/dapp-kit/dist/index.css';
 
 import ApiProvider from '@/component/api';
 import ThemeProvider from '@/component/theme/ThemeProvider';
@@ -11,15 +15,26 @@ import type { AppProps } from 'next/app';
 function App({ Component, pageProps }: AppProps) {
   const store: any = useStore();
 
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider>
-      <SnackbarProvider
-        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-      />
-      <PersistGate persistor={store.__persistor} loading={<></>}>
-        <ApiProvider>{<Component {...pageProps} />}</ApiProvider>
-      </PersistGate>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider
+        defaultNetwork="devnet"
+        networks={{ devnet: { url: getFullnodeUrl('devnet') } }}
+      >
+        <ThemeProvider>
+          <SnackbarProvider
+            anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+          />
+          <PersistGate persistor={store.__persistor} loading={<></>}>
+            <WalletProvider>
+              <ApiProvider>{<Component {...pageProps} />}</ApiProvider>
+            </WalletProvider>
+          </PersistGate>
+        </ThemeProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
   );
 }
 
