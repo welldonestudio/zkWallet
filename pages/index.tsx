a0e6c42f17a1c95cdffd9e7dbf34024057011961
daoauth <57783762+daoauth@users.noreply.github.com>
2023-12-19 18:55:40 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 
-import { useCurrentAccount, useCurrentWallet } from '@mysten/dapp-kit';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 
@@ -11,13 +10,7 @@ export default function HomePage() {
   const authState = useSelector(selectAuthState);
   const router = useRouter();
 
-  const { currentWallet } = useCurrentWallet();
-	const currentAccount = useCurrentAccount();
-
   useEffect(() => {
-    console.log(1, currentWallet);
-    console.log(2, currentAccount);
-  
     (!authState || !authState.jwt) && router.push('/signup');
   }, []);
 
