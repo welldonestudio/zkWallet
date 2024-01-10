05df3099e603bc1557ec8f9ab2ee93b5d02e1b8c
daoauth <57783762+daoauth@users.noreply.github.com>
2024-01-10 11:06:09 +0900

    Update stake.tsx

--- component/pages/wallet/stake.tsx
+++ component/pages/wallet/stake.tsx
@@ -1,25 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import SendIcon from '@mui/icons-material/Send';
-import {
-  Box,
-  Card,
-  CardContent,
-  CardHeader,
-  IconButton,
-  Tab,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Tabs,
-} from '@mui/material';
 import { useSelector } from 'react-redux';
 
 import { useContextApi } from '@/component/api';
-import SendTokenModal from '@/component/dialog/sendToken';
 import { selectAuthState } from '@/store/slice/authSlice';
 import { selectWalletState } from '@/store/slice/zkWalletSlice';
 
@@ -30,26 +13,7 @@ export const Stake = () => {
   const walletState = useSelector(selectWalletState);
   const { wallet } = useContextApi();
 
-  const [openSend, setOpenSend] = useState<boolean>(false);
   const [stakes, setStakes] = useState<ResponseStake[]>([]);
-
-  const handleSendConfirm = async (
-    password: string,
-    to: string,
-    amount: string,
-  ) => {
-    authState &&
-      (await wallet.sendToken({
-        auth: authState,
-        wallet: walletState.wallets[0],
-        password,
-        token: {
-          to,
-          type: '0x2::sui::SUI',
-          amount,
-        },
-      }));
-  };
 
   useEffect(() => {
     const update = async () => {
@@ -65,48 +29,5 @@ export const Stake = () => {
     walletState.wallets[0] && update();
   }, [walletState.wallets]);
 
-  return (
-    <>
-      <Card variant="outlined">
-        <CardHeader title="Tokens" />
-        <CardContent>
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Token</TableCell>
-                  <TableCell align="right">Balance</TableCell>
-                  <TableCell align="right"></TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {balances
-                  .filter((item) => item.type !== '0x2::sui::SUI')
-                  .map((item, key) => (
-                    <TableRow key={key}>
-                      <TableCell>{item.name}</TableCell>
-                      <TableCell align="right">{item.value}</TableCell>
-                      <TableCell align="right">
-                        <IconButton
-                          size="small"
-                          onClick={() => setOpenSend(true)}
-                        >
-                          <SendIcon fontSize="small" />
-                        </IconButton>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </CardContent>
-      </Card>
-      <SendTokenModal
-        title="Transfer Token"
-        open={openSend}
-        onClose={() => setOpenSend(false)}
-        confirm={handleSendConfirm}
-      />
-    </>
-  );
+  return <>test</>;
 };
